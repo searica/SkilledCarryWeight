40e019ab36bc00c0b709f743c6bae898753a482c
searica <143636061+searica@users.noreply.github.com>
2024-11-04 14:13:45 -0800

    Updated for Bog Witch

--- Publish/ThunderStore/CHANGELOG.md
+++ Publish/ThunderStore/CHANGELOG.md
@@ -5,6 +5,14 @@
 			<th align="center">Notes</th>
 		</tr>
 		<tr>
+			<td align="center">1.4.0</td>
+			<td align="left">
+				<ul>
+					<li>Updated for Bog Witch release.</li>
+				</ul>
+			</td>
+		</tr>
+		<tr>
 			<td align="center">1.3.0</td>
 			<td align="left">
 				<ul>
--- SkilledCarryWeight.cs
+++ SkilledCarryWeight.cs
@@ -18,7 +18,7 @@ namespace SkilledCarryWeight {
         internal const string Author = "Searica";
         public const string PluginName = "SkilledCarryWeight";
         public const string PluginGUID = $"{Author}.Valheim.{PluginName}";
-        public const string PluginVersion = "1.3.0";
+        public const string PluginVersion = "1.4.0";
 
         internal static readonly Dictionary<Skills.SkillType, SkillConfig> SkillConfigsMap = new();
 
